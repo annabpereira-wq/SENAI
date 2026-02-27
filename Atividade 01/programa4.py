918517c7c278ee294248643eb90a2f79fc79e893
annabpereira-wq <anna_b_pereira@estudante.sesisenai.org.br>
2026-02-27 19:43:57 +0000

    update

---
+++ Atividade 01/programa4.py
@@ -0,0 +1,9 @@
+nome = input("insira o seu nome")
+nota1 = float(input("insira o primeiro numero"))
+nota2 = float(input("insira o segundo numero"))
+media = (nota1 + nota2) / 2
+print(" A média é:", media)
+if media >= 7:
+    print(" o aluno esta aprovado")
+else:
+    print(" o aluno esta reprovado")
